b39aec80ce45175e26f94c36b074c84117e78c9a
Mario Konrad <mario.konrad@gmx.net>
2016-03-09 11:46:47 +0100

    Better configurability for plugin osm

--- shared/plugins/osm/osm.js
+++ shared/plugins/osm/osm.js
@@ -1,6 +1,32 @@
-var create_map = function(div_id, gpx_url, poi_url) {
+var make_gpx_layer = function(map, gpx_url) {
+	return new OpenLayers.Layer.Vector(
+		gpx_url,
+		{
+			strategies: [new OpenLayers.Strategy.Fixed()],
+			protocol: new OpenLayers.Protocol.HTTP({
+				url: gpx_url,
+				format: new OpenLayers.Format.GPX()
+			}),
+			style: {
+				strokeColor: "blue",
+				strokeWidth: 5,
+				strokeOpacity: 0.5
+			},
+			projection: map.displayProjection
+		});
+}
 
-	var map = new OpenLayers.Map('map', {
+var make_poi_layer = function(map, poi_url) {
+	return new OpenLayers.Layer.Text(
+		poi_url,
+		{
+			location: poi_url,
+			projection: map.displayProjection
+		});
+}
+
+var create_map = function(div_id, cfg) {
+	var map = new OpenLayers.Map(div_id, {
 		div: div_id,
 		projection: new OpenLayers.Projection("EPSG:900913"),
 		displayProjection: new OpenLayers.Projection("EPSG:4326"),
@@ -15,32 +41,32 @@ var create_map = function(div_id, gpx_url, poi_url) {
 
 	map.addLayer(new OpenLayers.Layer.OSM("OSM Map"));
 
-	map.addLayer(new OpenLayers.Layer.Vector(
-		gpx_url,
-		{
-			strategies: [new OpenLayers.Strategy.Fixed()],
-			protocol: new OpenLayers.Protocol.HTTP({
-				url: gpx_url,
-				format: new OpenLayers.Format.GPX()
-			}),
-			style: {
-				strokeColor: "blue",
-				strokeWidth: 5,
-				strokeOpacity: 0.5
-			},
-			projection: new OpenLayers.Projection("EPSG:4326")
-		}));
+	if ('gpx' in cfg) {
+		map.addLayer(make_gpx_layer(map, cfg['gpx']));
+	}
+	if ('poi' in cfg) {
+		map.addLayer(make_poi_layer(map, cfg['poi']));
+	}
 
-	map.addLayer(new OpenLayers.Layer.Text(
-		poi_url,
-		{
-			location: poi_url,
-			projection: map.displayProjection
-		}));
+	var lon = 0.0;
+	var lat = 0.0;
+	var zoom = 1;
+
+	if ('lon' in cfg) {
+		lon = 0.0 + cfg['lon'];
+	}
+	if ('lat' in cfg) {
+		lat = 0.0 + cfg['lat'];
+	}
+	if ('zoom' in cfg) {
+		zoom = 0 + cfg['zoom'];
+	}
 
 	map.setCenter(
-		new OpenLayers.LonLat(-2.5, 42.0).transform(new OpenLayers.Projection("EPSG:4326"),
-		map.getProjectionObject()), 5);
+		new OpenLayers.LonLat(lon, lat).transform(
+			new OpenLayers.Projection("EPSG:4326"),
+			map.getProjectionObject()),
+		zoom);
 
 	return map;
 }
