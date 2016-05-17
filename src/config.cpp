813e863bb9108d6752801e1577ae2d61d7c97e01
Mario Konrad <mario.konrad@gmx.net>
2016-05-17 12:40:32 +0200

    Config: themed config prepared

--- src/config.cpp
+++ src/config.cpp
@@ -121,6 +121,13 @@ public:
 		return result;
 	}
 
+	std::string get_site_title_background() const
+	{
+		return get_themed("site_title_background");
+	}
+
+	std::string get_copyright() const { return get_themed("copyright"); }
+
 private:
 	YAML::Node node_;
 
@@ -132,17 +139,24 @@ private:
 
 	std::string get_str(const std::string & tag, const std::string & default_value) const
 	{
-		return node_[tag] ? node_[tag].as<std::string>() : default_value;
+		return (node_[tag] && node_[tag].IsScalar()) ? node_[tag].as<std::string>()
+													 : default_value;
 	}
 
 	int get_int(const std::string & tag, int default_value) const
 	{
-		return node_[tag] ? node_[tag].as<int>() : default_value;
+		return (node_[tag] && node_[tag].IsScalar()) ? node_[tag].as<int>() : default_value;
 	}
 
 	bool get_bool(const std::string & tag, bool default_value) const
 	{
-		return node_[tag] ? node_[tag].as<bool>() : default_value;
+		return (node_[tag] && node_[tag].IsScalar()) ? node_[tag].as<bool>() : default_value;
+	}
+
+	std::string get_themed(const std::string field, const std::string & default_value = "") const
+	{
+		// TODO
+		return default_value;
 	}
 };
 }
@@ -169,21 +183,20 @@ int main(int argc, char ** argv)
 		return 0;
 	}
 
-	// TODO
 	mkweb::config cfg(config_filename);
 
-	std::cout << "source          =[" << cfg.get_source() << "]\n";
-	std::cout << "destination     =[" << cfg.get_destination() << "]\n";
-	std::cout << "static          =[" << cfg.get_static() << "]\n";
-	std::cout << "plugins         =[" << cfg.get_plugin_path() << "]\n";
-	std::cout << "site url        =[" << cfg.get_site_url() << "]\n";
-	std::cout << "plugin url      =[" << cfg.get_plugin_url() << "]\n";
-	std::cout << "site_title      =[" << cfg.get_site_title() << "]\n";
-	std::cout << "site_subtitle   =[" << cfg.get_site_subtitle() << "]\n";
-	std::cout << "language        =[" << cfg.get_language() << "]\n";
-	std::cout << "theme           =[" << cfg.get_theme() << "]\n";
-	std::cout << "author          =[" << cfg.get_author() << "]\n";
-	std::cout << "num_news        =[" << cfg.get_num_news() << "]\n";
+	std::cout << "source               =[" << cfg.get_source() << "]\n";
+	std::cout << "destination          =[" << cfg.get_destination() << "]\n";
+	std::cout << "static               =[" << cfg.get_static() << "]\n";
+	std::cout << "plugins              =[" << cfg.get_plugin_path() << "]\n";
+	std::cout << "site url             =[" << cfg.get_site_url() << "]\n";
+	std::cout << "plugin url           =[" << cfg.get_plugin_url() << "]\n";
+	std::cout << "site_title           =[" << cfg.get_site_title() << "]\n";
+	std::cout << "site_subtitle        =[" << cfg.get_site_subtitle() << "]\n";
+	std::cout << "language             =[" << cfg.get_language() << "]\n";
+	std::cout << "theme                =[" << cfg.get_theme() << "]\n";
+	std::cout << "author               =[" << cfg.get_author() << "]\n";
+	std::cout << "num_news             =[" << cfg.get_num_news() << "]\n";
 
 	std::cout << "source-process-filetypes:";
 	for (const auto & t : cfg.get_source_process_filetypes())
@@ -196,18 +209,21 @@ int main(int argc, char ** argv)
 				  << "}\n";
 	}
 
-	std::cout << "social-enable   =[" << cfg.get_social_enable() << "]\n";
-	std::cout << "social          =[" << cfg.get_social() << "]\n";
-	std::cout << "menu-enable     =[" << cfg.get_menu_enable() << "]\n";
-	std::cout << "menu            =[" << cfg.get_menu() << "]\n";
-	std::cout << "tags-enable     =[" << cfg.get_tags_enable() << "]\n";
-	std::cout << "page-tags-enable=[" << cfg.get_page_tags_enable() << "]\n";
-	std::cout << "years-enable    =[" << cfg.get_years_enable() << "]\n";
-	std::cout << "pagelist-enable =[" << cfg.get_pagelist_enable() << "]\n";
+	std::cout << "social-enable        =[" << cfg.get_social_enable() << "]\n";
+	std::cout << "social               =[" << cfg.get_social() << "]\n";
+	std::cout << "menu-enable          =[" << cfg.get_menu_enable() << "]\n";
+	std::cout << "menu                 =[" << cfg.get_menu() << "]\n";
+	std::cout << "tags-enable          =[" << cfg.get_tags_enable() << "]\n";
+	std::cout << "page-tags-enable     =[" << cfg.get_page_tags_enable() << "]\n";
+	std::cout << "years-enable         =[" << cfg.get_years_enable() << "]\n";
+	std::cout << "pagelist-enable      =[" << cfg.get_pagelist_enable() << "]\n";
 
 	const auto pagelist_sort = cfg.get_pagelist_sort();
-	std::cout << "pagelist-sort   = dir=" << static_cast<int>(pagelist_sort.dir)
+	std::cout << "pagelist-sort        = dir=" << static_cast<int>(pagelist_sort.dir)
 			  << ", key=" << pagelist_sort.key << '\n';
+
+	std::cout << "site-title-background=[" << cfg.get_site_title_background() << "]\n";
+	std::cout << "copyright            =[" << cfg.get_copyright() << "]\n";
 
 	return EXIT_SUCCESS;
 }
