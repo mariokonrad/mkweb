c720ae03288eed972a6c021e2d4fb1cfb45cc661
Mario Konrad <mario.konrad@gmx.net>
2017-07-09 18:50:05 +0000

    Compiler set to GCC-7.

--- bin/compiler-env.sh
+++ bin/compiler-env.sh
@@ -1,9 +1,2 @@
 #!/bin/bash -eu
-
-## GCC-7
-#export CMAKE_CXX_COMPILER=${HOME}/tmp/gcc-7/bin/g++
-#export LD_LIBRARY_PATH=${HOME}/tmp/gcc-7/lib64
-
-# GCC-6
-export CMAKE_CXX_COMPILER=g++-6
-
+export CMAKE_CXX_COMPILER=g++-7
